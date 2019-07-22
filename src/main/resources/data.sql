466d91e67eeeb71a3720f17924e26df9617f4d78
Gaurav.Maskara <Gaurav.Maskara@IN2363675W0.ey.net>
2019-07-22 13:49:43 +0530

    changes

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,6 @@
 insert into employee(employeeId, employeeName , employeeAddress,employeeEmail) values('1','Gaurav','India','gaurav@gmail.com');
 
 insert into users(username, useraddress, useremail, userpassword) values ('gaurav', 'india', 'gaurav@gmail.com', 'gaurav');
+
+
+truncate table VisitorDetails;

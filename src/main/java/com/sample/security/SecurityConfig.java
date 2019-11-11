1a321df00d1587186f3384e1799bed9ef959497d
Gaurav.Maskara <Gaurav.Maskara@IN2363675W0.ey.net>
2019-11-11 11:57:45 +0530

    removing password protection

--- src/main/java/com/sample/security/SecurityConfig.java
+++ src/main/java/com/sample/security/SecurityConfig.java
@@ -1,27 +1,29 @@
-
-package com.sample.security;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-@Configuration
-
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
-		http.cors().and().csrf().disable();
-	}
-
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("admin").password("{noop}test123").roles("ADMIN").and().withUser("user")
-				.password("{noop}user").roles("USER");
-	}
-
-}
+/*
+ * package com.sample.security;
+ * 
+ * import org.springframework.context.annotation.Configuration; import
+ * org.springframework.security.config.annotation.authentication.builders.
+ * AuthenticationManagerBuilder; import
+ * org.springframework.security.config.annotation.web.builders.HttpSecurity;
+ * import org.springframework.security.config.annotation.web.configuration.
+ * EnableWebSecurity; import
+ * org.springframework.security.config.annotation.web.configuration.
+ * WebSecurityConfigurerAdapter;
+ * 
+ * @Configuration
+ * 
+ * @EnableWebSecurity public class SecurityConfig extends
+ * WebSecurityConfigurerAdapter {
+ * 
+ * @Override protected void configure(HttpSecurity http) throws Exception {
+ * http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+ * http.cors().and().csrf().disable(); }
+ * 
+ * @Override protected void configure(AuthenticationManagerBuilder auth) throws
+ * Exception {
+ * auth.inMemoryAuthentication().withUser("admin").password("{noop}test123").
+ * roles("ADMIN").and().withUser("user") .password("{noop}user").roles("USER");
+ * }
+ * 
+ * }
+ */
